58f8aa7147bb437c92cecc7e1706e07c5db8b613
Markus Ritzmann <git@markus-mail.com>
2020-06-01 18:14:11 +0200

    Revert "move config to ci config"
    This reverts commit 47836081a8669b2cd1f340fbfe9775536cba051b.

---
+++ lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ci: {
+      collect: {
+        url: ['https://markusritzmann.ch/'],
+      },
+      upload: {
+        target: 'temporary-public-storage',
+      },
+    },
+  };
